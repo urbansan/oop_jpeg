d848664e4da18d7d43b935031fdbaedfcd14ef46
Urbansan <urbansanek@gmail.com>
2021-06-20 09:30:05 +0200

    reformatted

--- src/oop_jpeg/frame_readers/dqt.py
+++ src/oop_jpeg/frame_readers/dqt.py
@@ -7,17 +7,16 @@ from .markers import Marker
 class DqtReader(AbstractReader):
     marker = Marker.DQT
 
-
     def _parse_bytes(self, bytes_: List[int]):
         DQTs = []
         position = 0
         while len(bytes_) >= position + 1:
             table_id = bytes_[position] & 0xF
             if bytes_[position] >> 4 == 1:
-                data = self._8bit_to_16bit_step(bytes_[position + 1: position + 129])
+                data = self._8bit_to_16bit_step(bytes_[position + 1 : position + 129])
                 position += 129
             else:
-                data = bytes_[position + 1: position + 65]
+                data = bytes_[position + 1 : position + 65]
                 position += 65
             dqt = DQT.from_byte_stream(table_id, data)
             DQTs.append(dqt)
@@ -37,14 +36,70 @@ class DqtReader(AbstractReader):
 
 class DQT(list):
     zig_zag_indexes = [
-        (0, 0), (0, 1), (1, 0), (2, 0), (1, 1), (0, 2), (0, 3), (1, 2),
-        (2, 1), (3, 0), (4, 0), (3, 1), (2, 2), (1, 3), (0, 4), (0, 5),
-        (1, 4), (2, 3), (3, 2), (4, 1), (5, 0), (6, 0), (5, 1), (4, 2),
-        (3, 3), (2, 4), (1, 5), (0, 6), (0, 7), (1, 6), (2, 5), (3, 4),
-        (4, 3), (5, 2), (6, 1), (7, 0), (7, 1), (6, 2), (5, 3), (4, 4),
-        (3, 5), (2, 6), (1, 7), (2, 7), (3, 6), (4, 5), (5, 4), (6, 3),
-        (7, 2), (7, 3), (6, 4), (5, 5), (4, 6), (3, 7), (4, 7), (5, 6),
-        (6, 5), (7, 4), (7, 5), (6, 6), (7, 1), (6, 7), (7, 6), (7, 7),
+        (0, 0),
+        (0, 1),
+        (1, 0),
+        (2, 0),
+        (1, 1),
+        (0, 2),
+        (0, 3),
+        (1, 2),
+        (2, 1),
+        (3, 0),
+        (4, 0),
+        (3, 1),
+        (2, 2),
+        (1, 3),
+        (0, 4),
+        (0, 5),
+        (1, 4),
+        (2, 3),
+        (3, 2),
+        (4, 1),
+        (5, 0),
+        (6, 0),
+        (5, 1),
+        (4, 2),
+        (3, 3),
+        (2, 4),
+        (1, 5),
+        (0, 6),
+        (0, 7),
+        (1, 6),
+        (2, 5),
+        (3, 4),
+        (4, 3),
+        (5, 2),
+        (6, 1),
+        (7, 0),
+        (7, 1),
+        (6, 2),
+        (5, 3),
+        (4, 4),
+        (3, 5),
+        (2, 6),
+        (1, 7),
+        (2, 7),
+        (3, 6),
+        (4, 5),
+        (5, 4),
+        (6, 3),
+        (7, 2),
+        (7, 3),
+        (6, 4),
+        (5, 5),
+        (4, 6),
+        (3, 7),
+        (4, 7),
+        (5, 6),
+        (6, 5),
+        (7, 4),
+        (7, 5),
+        (6, 6),
+        (7, 1),
+        (6, 7),
+        (7, 6),
+        (7, 7),
     ]
 
     def __init__(self, table_id):
@@ -60,9 +115,9 @@ class DQT(list):
         return dqt
 
     def __repr__(self):
-        return f'{type(self).__name__}(table_id={self.id})'
+        return f"{type(self).__name__}(table_id={self.id})"
 
-    #stream_zigzag = [
+    # stream_zigzag = [
     #     0, 1, 8, 16, 9, 2, 3, 10,
     #     17, 24, 32, 25, 18, 11, 4, 5,
     #     12, 19, 26, 33, 40, 48, 41, 34,
@@ -72,5 +127,3 @@ class DQT(list):
     #     58, 59, 52, 45, 38, 31, 39, 46,
     #     53, 60, 61, 54, 57, 55, 62, 63,
     # ]
-
-
